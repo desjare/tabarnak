9974b663a55c797d0fde5d16952612a42d1d762e
Eric Desjardins <desjare@gmail.com>
2020-10-29 06:55:57 -0400

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tabarnak-desjare", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Eric Desjardins",
     author_email="desjare@gmail.com",
     description="transcoder FFmpeg based wrapper",
